67add517bc9ce264f2c9dff6d0eb1a3f7839c19d
Nicolas Kirsch <nicolas.kirsch18@gmail.com>
2020-10-28 14:13:08 +0100

    notebook

--- NoNull.py
+++ NoNull.py
@@ -11,6 +11,12 @@ import numpy as np
 
 df = pd.read_csv(r"..\Data\Londondata\Power-Networks-LCL-June2015(withAcornGps)v2_34.csv")
 
+for i in range(1,3): 
+    dataframe = pd.read_csv(r"..\Data\Londondata\Power-Networks-LCL-June2015(withAcornGps)v2_"+str(i)+".csv")
+    df = df.append(dataframe)
+
+
+
 #Get the year of observation
 df["year"] = pd.to_datetime(df["DateTime"]).dt.year
 df.index = df.index - 3840
@@ -22,18 +28,3 @@ df = df[df["year"] == 2013]
 #Group by LCLid and keep only those with enough entries in 2013
 df['KWH/hh (per half hour) '] = df['KWH/hh (per half hour) '].replace('Null',np.nan)
 groups = df.groupby("LCLid")['KWH/hh (per half hour) '].apply(lambda x: len(x))
-groups = groups[groups >=17520]
-l = list(groups.index)
-df = df[df["LCLid"].isin(l)]
-
-
-
-#Change format of dataframe, have Time as col and person as row
-e = df.groupby('LCLid')['KWH/hh (per half hour) '].apply(lambda df: df.reset_index(drop=True)).unstack()
-
-
-#Give column name
-time =  df.iloc[range(0,17532),2]
-e.columns = time
-
-print("fffffffff")
